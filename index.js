6ef22b2c90abb5798e0398c282c69ffeeedd051b
Yuji Kurihara <syaro@gochiusa.net>
2017-04-23 22:44:57 +0900

    add Window Setting

--- index.js
+++ index.js
@@ -46,17 +46,10 @@ function CreateMenu() {
 				{ type: "separator" },
 				{
 					label: 'ProfileSettings',
-					click: function () { openLinks("https://mstdn.awa.sfc.keio.ac.jp/settings/profile"); }
-				},
-				{
-					label: 'UserSettings',
+					accelerator: 'Command+,',
 					click: function () { openLinks("https://mstdn.awa.sfc.keio.ac.jp/settings/preferences"); }
 				},
-				{
-					label: 'Reload',
-					accelerator: 'Command+R',
-					click: function () { reload(); }
-				},
+				{ type: "separator" },
 				{
 					label: 'Quit',
 					accelerator: 'Command+Q',
@@ -66,13 +59,77 @@ function CreateMenu() {
 		}, {
 			label: "Edit",
 			submenu: [
-				{ label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-				{ label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+				{
+					label: "Undo",
+					accelerator: "CmdOrCtrl+Z",
+					selector: "undo:"
+				},
+				{
+					label: "Redo",
+					accelerator: "Shift+CmdOrCtrl+Z",
+					selector: "redo:"
+				},
 				{ type: "separator" },
-				{ label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-				{ label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-				{ label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-				{ label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+				{
+					label: "Cut",
+					accelerator: "CmdOrCtrl+X",
+					selector: "cut:"
+				},
+				{
+					label: "Copy",
+					accelerator: "CmdOrCtrl+C",
+					selector: "copy:"
+				},
+				{
+					label: "Paste",
+					accelerator: "CmdOrCtrl+V",
+					selector: "paste:"
+				},
+				{
+					label: "Select All",
+					accelerator: "CmdOrCtrl+A",
+					selector: "selectAll:"
+				}
+			]
+		}, {
+			label: "Window",
+			submenu: [
+				{
+					label: 'Reload',
+					accelerator: 'Command+R',
+					click: function () { reload(); }
+				},
+				{
+					type: 'separator',
+				},
+				{
+					role: 'resetzoom',
+				},
+				{
+					role: 'zoomin',
+				},
+				{
+					role: 'zoomout',
+				},
+				{
+					type: 'separator',
+				},
+				{
+					role: 'togglefullscreen',
+				}
+			]
+		}, {
+			label: "Help",
+			submenu: [
+				{
+					label: 'Update Software',
+					click: function () { openLinks("https://github.com/syaro/tootee/releases"); }
+				},
+				{ type: "separator" },
+				{
+					label: 'Send Feedback',
+					click: function () { openLinks("https://mstdn.awa.sfc.keio.ac.jp/@syaro"); }
+				}
 			]
 		}
 	];
